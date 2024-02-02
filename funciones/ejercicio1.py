6a3b6c23787cb0cca35f94537dee66eb03f35133
cristopher-loyola <157190820+cristopher-loyola@users.noreply.github.com>
2024-02-01 23:53:49 -0600

    practica 6 terminada

--- funciones/ejercicio1.py
+++ funciones/ejercicio1.py
@@ -2,3 +2,15 @@
 # el IVA. La función debe recibir la cantidad sin IVA y el porcentaje de
 # IVA a aplicar, y devolver el total de la factura. Si se invoca la función
 # sin pasarle el porcentaje de IVA, deberá aplicar un 21%.
+
+def factura(no_iva, porcentaje_iva=21):
+    total = no_iva + (no_iva * porcentaje_iva /100)
+    return total
+no_iva = float(input("ingresa la cantidad sin iva: "))
+porcentaje_persona = input("ingrese el porcentaje del iva: ")
+if porcentaje_persona:
+    porcentaje_iva = float(porcentaje_persona)
+else:
+    porcentaje_iva = 21
+total = factura(no_iva, porcentaje_iva)
+print("El total de la factura es: ", total)
