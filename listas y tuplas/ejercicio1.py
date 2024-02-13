0d6e0344ea9e496b10ecded77f1903b90eae7a83
cristopher-loyola <157190820+cristopher-loyola@users.noreply.github.com>
2024-02-12 20:30:11 -0600

    ejercicio no terminado

---
+++ listas y tuplas/ejercicio1.py
@@ -0,0 +1,18 @@
+# Crea un programa usando funciones y lo que necesites para que el usuario inserte
+# N números en una Tupla, después de la captura debe desplegar el siguiente menú
+# funcional
+# 1. Sumatoria de los elementos de la lista
+# 2. Numero mayor de la lista
+# 3. Numero menor de la lista
+# 4. Promedio
+# 5. Moda: es el valor que más se repite de un conjunto de datos
+# 6. Rango: es la diferencia entre el valor máximo y el valor mínimo de un
+# conjunto de datos
+
+# numeros = (input("ingrese una lista de numeros: "))
+# tuple = numeros
+# print(tuple)
+
+numeros = input("ingrese una lista de numeros: ")
+list = numeros
+print(list)
