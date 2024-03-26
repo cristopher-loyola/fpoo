b106905af3c0c9118809643f1ca2571b1f579013
cristopher-loyola <157190820+cristopher-loyola@users.noreply.github.com>
2024-03-25 22:34:46 -0600

    hemos llegado hasta aqui en la clase

--- tkSQLite/controlador.py
+++ tkSQLite/controlador.py
@@ -1,13 +1,13 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, Text, Tk, ttk
 import sqlite3
 import bcrypt
 
-class Controlador: 
+class controlador:
 
     def conexion(self):
         try:
-            conex = sqlite3.connect("C:/Users/dieco/OneDrive/Documentos/Fundamentos de programacion/FPOO195/TK-SQLITE/db195.db")
+            conex = sqlite3.connect("C:\\Users\\USUARIO\\Documents\\github\\fpoo\\tkSQLite\\db195.db")
             print("Conectado")
             return conex
         except sqlite3.OperationalError:
@@ -21,16 +21,38 @@ class Controlador:
         return passHash
     
     def insertUsuario(self, nom, corr, cont):
-        conexion = self.conexion()
         if nom == "" or corr == "" or cont == "":
-            messagebox.showwarning("Cuidado", "Inputs vacíos no sea tibio")
-            conexion.close()
+            messagebox.showerror("Error", "¡Inputs vacíos no son permitidos!")
+            return
+        
+        conH = self.encriptapass(cont)
+        datos = (nom, corr, conH)
+        sqlInsert = "INSERT INTO tbUsuarios(nombre, correo, contra) VALUES (?,?,?)"
+        
+        try:
+            with self.conexion() as conexion:
+                cursor = conexion.cursor()
+                cursor.execute(sqlInsert, datos)
+                conexion.commit()
+                messagebox.showinfo("Éxito", "Usuario insertado exitosamente")
+        except sqlite3.Error as e:
+            messagebox.showerror("Error", f"No se pudo insertar el usuario: {e}")
+            
+    def buscarUsuario(self, id):
+        conex = self.conexion()
+        if id == '':
+            messagebox.showwarning("Cuidado", "inputs vacíos")
+            conex.close()
         else:
-            cursor = conexion.cursor()
-            conH = self.encriptapass(cont)
-            datos = (nom, corr, conH)
-            sqlInsert = "INSERT INTO tbUusarios(nombre, correo, contrasena) VALUES (?,?,?)"
-            cursor.execute(sqlInsert, datos)
-            conexion.commit()
-            conexion.close()
-            messagebox.showinfo("Éxito", "Eso tilin!!!!!!!!!")
+            try:
+                cursor = conex.cursor()
+                sqlSelect = f"SELECT * FROM tbUsuarios WHERE id={id}"
+                cursor.execute(sqlSelect)
+                usuario = cursor.fetchone()
+                conex.close()
+                if usuario:
+                    return usuario
+                else:
+                    messagebox.showwarning("Nada", "Id no existe en BD")
+            except sqlite3.OperationalError:
+                print("No se pudo ejecutar la búsqueda")
--- tkSQLite/notebookCRud.py
+++ tkSQLite/notebookCRud.py
@@ -1,13 +1,20 @@
 from tkinter import *
-from tkinter import ttk
+from tkinter import ttk, messagebox
 import tkinter as tk
-from contolador import Controlador  # Corregido el nombre de la clase
+from controlador import controlador
 
-
-objControlador = Controlador()  # Corregido el nombre de la clase
+objControlador = controlador()
 
 def ejecutarInsert():
     objControlador.insertUsuario(var1.get(), var2.get(), var3.get())
+    
+def buscarUsuario():
+    usuarioBD = objControlador.buscarUsuario(varBus.get())
+    if not usuarioBD:
+        messagebox.showwarning("Nada", "Id no existe en BD")
+    else:
+        text_result.delete(1.0, END)
+        text_result.insert(END, usuarioBD)
 
 Ventana = Tk()
 Ventana.title("CRUD de usuarios")
@@ -16,8 +23,20 @@ Ventana.geometry("500x300")
 panel = ttk.Notebook(Ventana)
 panel.pack(fill='both', expand='yes')
 
-pestana1 = ttk.Frame(panel)
-panel.add(pestana1, text="Crear usuario")
+#3 definir las pestañas del nootbook
+pestana1= ttk.Frame(panel)
+pestana2= ttk.Frame(panel)
+pestana3= ttk.Frame(panel)
+pestana4= ttk.Frame(panel)
+pestana5= ttk.Frame(panel)
+
+
+#4 agregamos las pestañas
+panel.add(pestana1,text="Crear usuario")
+panel.add(pestana2,text="Buscar usuario")
+panel.add(pestana3,text="Consultar usuario")
+panel.add(pestana4,text="Editar usuario")
+panel.add(pestana5,text="Eliminar usuario")
 
 Label(pestana1, text="Registro de usuarios", fg="blue", font=("Modern", 18)).pack()
 
@@ -34,4 +53,16 @@ Label(pestana1, text="Contraseña: ").pack()
 Entry(pestana1, textvariable=var3).pack()
 
 Button(pestana1, text="Guardar usuario", command=ejecutarInsert).pack()
+
+Label(pestana2, text="Buscar usuario", fg="red", font=("Mono", 18)).pack()
+varBus = tk.StringVar()
+Label(pestana2, text="id: ").pack()
+Entry(pestana2, textvariable=varBus).pack()
+
+Button(pestana2, text="buscarUsuario", command=buscarUsuario).pack()
+
+Label(pestana2, text="Registrado: ", fg="blue", font=("Mono", 16)).pack()
+text_result = Text(pestana2, height=5, width=52)
+text_result.pack()
+
 Ventana.mainloop()
